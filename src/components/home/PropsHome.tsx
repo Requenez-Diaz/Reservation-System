669d6a83044c36101fc8a5fc2737e1db8e2b522f
Avimilex <95236561+Requenez-Diaz@users.noreply.github.com>
2024-03-11 14:44:07 -0600

    [add]: popover in the prinsipal page home

--- src/components/home/PropsHome.tsx
+++ src/components/home/PropsHome.tsx
@@ -1,54 +1,41 @@
-// 'use client';
-
-import { CalendarIcon, User } from 'lucide-react';
 import React from 'react';
-import { ChevronDown } from 'lucide-react';
+import { User } from 'lucide-react';
 import { Button } from '@/components/ui/button';
-
-interface PropsHomeProps {
-  date: string;
-  peoples: string;
-}
+import { CalendarForm } from './cards/calendar';
+import { PopoverForms } from './cards/popover';
 
 const PropsHome = () => {
   return (
-    <div className="bg-blue-400 w-3/4 p-2 m-4 flex flex-col mx-auto content-center rounded-xl shadow-2xl shadow-black">
-      <div className="text-white text-center">
-        <h1 className="font-bold">Selecciona una habitacion</h1>
-        <p>Descubre el mejor lugar para ti!!</p>
-      </div>
+    <div className="flex justify-center items-center p-4">
+      <div className="bg-gray-400 w-2/3 p-4 flex flex-col items-center rounded-xl shadow-2xl">
+        <div className="text-white text-center">
+          <h1 className="font-bold text-2xl">Selecciona una habitación</h1>
+          <p>Descubre el mejor lugar para ti!!</p>
+        </div>
 
-      <div className="mt-4">
-        <div className="flex flex-row m-2 justify-between items-center">
-          <div>
-            <h2>Fecha</h2>
-            <div className="flex flex-row m-2 justify-between items-center content-center bg-gray-300 p-2 rounded-lg">
-              <p className="mb-auto mr-4">Seleccionar Fecha</p>
-              <CalendarIcon className="my-auto" />
+        <div className="mt-4 w-full flex flex-col md:flex-row md:justify-between">
+          <div className="w-full md:w-2/3 mb-4 md:mb-0 flex flex-col md:flex-row justify-center md:justify-between items-center p-2 rounded-lgml-8">
+            <div className="w-full md:w-1/3 mb-4 md:mb-0">
+              <div className="flex items-center justify-center">
+                <CalendarForm />
+              </div>
+            </div>
+            <div className="flex items-center mb-2 md:mb-0 ml-8">
+              <User className="mr-1 " />
+              <p className="mb-0 mr-7 md:ml-4">Adultos</p>
+              <PopoverForms />
+            </div>
+            <div className="flex items-center ml-8">
+              <User className="mr-1" />
+              <p className="mb-0 mr-7 md:ml-4">Niños</p>
+              <PopoverForms />
             </div>
           </div>
-
-          <div>
-            <h2 className="text-white">Personas</h2>
-            <div className="flex flex-row m-2 justify-between items-center content-center bg-gray-300  p-2 rounded-lg">
-              <div className="flex items-center">
-                <User className="mr-2" />
-                <p className="mb-auto mr-4">Adultos</p>
-              </div>
-              <ChevronDown />
-              <div className="flex items-center">
-                <User className="mr-2" />
-                <p className="mb-auto">Niños</p>
-              </div>
-              <ChevronDown />
-            </div>
-          </div>
-
-          <div>
-            <Button className="bg-blue-700  hover:bg-black">
-              Buscar Habitacion
-            </Button>
-          </div>
+        </div>
+        <div className="w-full md:w-1/3 flex justify-center mt-4 md:mt-0 mx-auto">
+          <Button className="bg-blue-700 hover:bg-black">
+            Buscar Habitación
+          </Button>
         </div>
       </div>
     </div>
--- src/components/home/bannerHome.tsx
+++ src/components/home/bannerHome.tsx
@@ -1,21 +1,21 @@
+import Image from 'next/image';
 import React from 'react';
 
 const BannerHome = () => {
   return (
     <div className="relative w-full h-96">
-      <img
+      <Image
         alt="vercel logo"
         className="object-cover w-full h-full opacity-80"
         height={1080}
         src="/pexels-helena-lopes-2017802.jpg"
         width={1920}
       />
-      <div className="absolute top-1/2 left-1/3 right-1 ml-4 transform -translate-x-1/2 -translate-y-1/2 text-white bg-gray-300 flex flex-col items-center justify-center rounded-r-2xl h-1/2">
+      <div className="absolute top-1/2 left-1/3 right-1 ml-4 transform -translate-x-1/2 -translate-y-1/2 text-white bg-gray-400 flex flex-col items-center justify-center rounded-r-2xl h-1/2">
         <h1 className="text-4xl font-bold ">Bienvenido a SIRHM</h1>
         <p className="text-black ml-3 mt-4 justify-center items-center">
-          Lorem ipsum dolor sit amet consectetur, adipisicing elit. Delectus
-          dolor officia natus quod inventore voluptatem dicta vero sit vel
-          facilis, quia quae, at voluptas.
+          Este es uno de los sistemas de reservas de hoteles más avanzados de
+          Nueva Guinea donde todo lo encontraras a un click
         </p>
       </div>
     </div>
