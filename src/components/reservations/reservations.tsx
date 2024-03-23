82ad82ab50dcc66faf05f38180ff4059cc6cc641
Elliam Sánchez <elliamsanchez510@gmail.com>
2024-03-22 23:25:15 -0600

    Reservation Form with Data Handling

--- src/components/reservations/reservations.tsx
+++ src/components/reservations/reservations.tsx
@@ -1,10 +1,11 @@
 import React from 'react';
+import ReservationForm from './reservationForm';
 
 const Reservas: React.FC = () => {
     return (
         <div className="container mx-auto px-4 py-8">
             <div className="text-center mb-8">
-                <h2 className="text-3xl font-bold text-blue-600 mb-2">Reservas</h2>
+                <h2 className="text-3xl font-bold text-blue-600 mb-2">Reservasciones</h2>
                 <p className="text-lg text-gray-700">¡Gracias por elegir nuestro hotel para tu próxima estancia! Estamos emocionados de recibirte. A continuación, encontrarás información sobre cómo realizar una reserva y nuestras políticas de cancelación.</p>
             </div>
 
@@ -28,6 +29,7 @@ const Reservas: React.FC = () => {
                     <li>En caso de cancelación, se emitirá un reembolso completo si la reserva se cancela con al menos 48 horas de anticipación a la fecha de llegada.</li>
                 </ul>
             </div>
+            <ReservationForm/>
         </div>
     );
 };
