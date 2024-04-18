4790b8b02311c8a3e244a51e30ad53a34e0c2bee
Darwin Salinas <salinash2000@gmail.com>
2024-04-18 14:54:12 -0600

    Remove unused imports

--- src/components/offers/cardsComponents.tsx
+++ src/components/offers/cardsComponents.tsx
@@ -1,6 +1,4 @@
-import React from 'react';
-import OfertsHeader from './ofertsHeader';
-import OfertsProps from './OfferProperty';
+import OfertsHeader from './OfferHeader';
 import SpecialOferts from './SpecialOffer';
 
 const CardsComponents = () => {
