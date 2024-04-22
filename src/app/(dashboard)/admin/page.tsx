88a3761d542ce9fddc002707c990130d09dbb442
Avimilex <95236561+Requenez-Diaz@users.noreply.github.com>
2024-04-22 16:13:34 -0600

    [add]: get all users loged in the database

--- src/app/(dashboard)/admin/page.tsx
+++ src/app/(dashboard)/admin/page.tsx
@@ -8,12 +8,13 @@ const Dashboard = async () => {
   if (session?.user) {
     return (
       <h2 className="text-2xl">
-        Admin Page - welcome back {session?.user.username}
+        {session?.user.username}
+        <ContainerHome />
       </h2>
     );
   }
 
-  return <ContainerHome />;
+  // return <ContainerHome />;
 };
 
 export default Dashboard;
