c8949b13e415dd576552aed1a3763d4f8fe70d25
Elliam Sánchez <elliamsanchez510@gmail.com>
2024-06-21 16:12:48 -0600

    Implemented responsive design for PropsCards

--- src/components/offers/SpecialOffers.tsx
+++ src/components/offers/SpecialOffers.tsx
@@ -74,7 +74,7 @@ export const SpecialOffers = () => {
   ];
 
   return (
-    <div className="p-8 grid grid-cols-4 gap-4">
+    <div className="p-4 grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-4">
       {containerImage.map((ofert, index) => (
         <OfferProperty
           description={ofert.description}
