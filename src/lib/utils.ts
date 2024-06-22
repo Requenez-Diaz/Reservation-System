76d9f7cb1bd85ded241284526504859bcb2a0803
Requenez <95236561+Requenez-Diaz@users.noreply.github.com>
2024-06-22 17:22:08 -0600

    [fix]: resolving error to register to redirect to home

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -1,16 +1,16 @@
-import { type ClassValue, clsx } from "clsx"
-import { twMerge } from "tailwind-merge"
+import { type ClassValue, clsx } from 'clsx';
+import { twMerge } from 'tailwind-merge';
 
 export function cn(...inputs: ClassValue[]) {
-  return twMerge(clsx(inputs))
+  return twMerge(clsx(inputs));
 }
-
 
 export const formaData = (dateStr: string) => {
   const data = new Date(dateStr);
-  const formatter = new Intl.DateTimeFormat("id-ID", {
-    dateStyle: "medium",
-    timeStyle: "short",
+  console.log(data);
+  const formatter = new Intl.DateTimeFormat('id-ID', {
+    dateStyle: 'medium',
+    timeStyle: 'short'
   });
   return formatter.format(data);
 };
