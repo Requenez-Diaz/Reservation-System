8158fbbd55f892a966cdad1697b1bd63747805a8
Elliam Sánchez <elliamsanchez510@gmail.com>
2024-09-08 20:19:45 -0600

    feat: create server action to save room reservations

--- src/components/bedrooms/formReservation.tsx
+++ src/components/bedrooms/formReservation.tsx
@@ -1,115 +1,102 @@
 'use client';
 
-import { saveBookings } from '@/app/actions/bookings/booking';
+import { saveReservation } from '@/app/actions/saveReservation/saveReservation';
 import { bedroomsTypes } from '@/components/bedroomstype/bedroomsType';
 import { Button } from '@/components/ui/button';
 import { DialogClose, DialogFooter } from '@/components/ui/dialog';
 import { Input } from '@/components/ui/input';
 import { Label } from '@/components/ui/label';
+import { useToast } from "@/components/ui/use-toast";
 
 export function FormReservation() {
+    const { toast } = useToast();
 
-    const handleSubmit = async (e: React.FormEvent<HTMLFormElement>) => {
-        e.preventDefault();
+    const handleSubmit = async (event: React.FormEvent<HTMLFormElement>) => {
+        event.preventDefault();
+        const response = await saveReservation(new FormData(event.currentTarget));
 
-        const formData = new FormData(e.currentTarget);
-
-        const arrivalDate = formData.get('arrivalDate') as string;
-        const departureDate = formData.get('departureDate') as string;
-
-        if (new Date(arrivalDate) >= new Date(departureDate)) {
-            alert('La fecha de salida debe ser posterior a la fecha de llegada.');
-            return;
+        if (response.success) {
+            toast({
+                title: "Reserva realizada.",
+                description: "La reservación se registró correctamente.",
+            });
+        } else {
+            toast({
+                title: "Reserva no realizada.",
+                description: "Ha ocurrido un error al realizar la reservación.",
+            });
         }
-
-        await saveBookings(formData);
     };
 
     return (
-        <form onSubmit={handleSubmit} className="space-y-6">
-            <div className="grid grid-cols-4 items-center gap-4">
-                <Label className="text-right" htmlFor="guests">
-                    Personas
-                </Label>
-                <Input
-                    className="col-span-3"
-                    id="guests"
-                    name="guests"
-                    type="number"
-                    min="1"
-                    placeholder="Número de personas"
-                    required
-                />
+        <form onSubmit={handleSubmit} className="space-y-4">
+
+            <div className="grid grid-cols-2 gap-4">
+                <div>
+                    <Label className="text-right" htmlFor="name">Nombre</Label>
+                    <Input id="name" name="name" type="text" placeholder='Nombre' required />
+                </div>
+                <div>
+                    <Label className="text-right" htmlFor="lastName">Apellido</Label>
+                    <Input id="lastName" name="lastName" type="text" placeholder='Apellido' required />
+                </div>
             </div>
-            <div className="grid grid-cols-4 items-center gap-4">
-                <Label className="text-right" htmlFor="rooms">
-                    Habitaciones
-                </Label>
-                <Input
-                    className="col-span-3"
-                    id="rooms"
-                    name="rooms"
-                    type="number"
-                    min="1"
-                    placeholder="Cantidad de habitaciones"
-                    required
-                />
+
+            <div className="grid grid-cols-2 gap-4">
+                <div>
+                    <Label className="text-right" htmlFor="email">Correo</Label>
+                    <Input id="email" name="email" type="email" placeholder='Correo electronico' required />
+                </div>
+                <div>
+                    <Label className="text-right" htmlFor="guests">Huéspedes</Label>
+                    <Input id="guests" name="guests" type="number" min="1" placeholder="Número de personas" required />
+                </div>
             </div>
-            <div className="grid grid-cols-4 items-center gap-4">
-                <Label className="text-right" htmlFor="roomType">
-                    Tipo de habitación
-                </Label>
-                <select
-                    className="col-span-3 border border-gray-300 rounded-lg p-2"
-                    id="roomType"
-                    name="roomType"
-                    required
-                >
-                    <option value="" disabled>
-                        Selecciona el tipo de habitación
-                    </option>
-                    {bedroomsTypes.map((type, index) => (
-                        <option key={index} value={type}>
-                            {type}
-                        </option>
-                    ))}
-                </select>
+
+            <div className="grid grid-cols-2 gap-4">
+                <div>
+                    <Label className="text-right" htmlFor="rooms">Habitaciones</Label>
+                    <Input id="rooms" name="rooms" type="number" min="1" placeholder="Cantidad de habitaciones" required />
+                </div>
+                <div>
+                    <Label className="text-right" htmlFor="bedroomsType">Tipo de habitación</Label>
+                    <select className="border border-gray-300 rounded-lg p-2" id="bedroomsType" name="bedroomsType" required>
+                        <option value="" disabled>Selecciona el tipo de habitación</option>
+                        {bedroomsTypes.map((type, index) => (
+                            <option key={index} value={type}>{type}</option>
+                        ))}
+                    </select>
+                </div>
             </div>
-            <div className="grid grid-cols-4 items-center gap-4">
-                <Label className="text-right" htmlFor="arrivalDate">
-                    Llegada
-                </Label>
-                <Input
-                    className="col-span-3"
-                    id="arrivalDate"
-                    name="arrivalDate"
-                    type="date"
-                    required
-                />
+
+            <div className="grid grid-cols-2 gap-4">
+                <div>
+                    <Label className="text-right" htmlFor="arrivalDate">Fecha de llegada</Label>
+                    <Input id="arrivalDate" name="arrivalDate" type="date" required />
+                </div>
+                <div>
+                    <Label className="text-right" htmlFor="departureDate">Fecha de salida</Label>
+                    <Input id="departureDate" name="departureDate" type="date" required />
+                </div>
             </div>
-            <div className="grid grid-cols-4 items-center gap-4">
-                <Label className="text-right" htmlFor="departureDate">
-                    Salida
-                </Label>
-                <Input
-                    className="col-span-3"
-                    id="departureDate"
-                    name="departureDate"
-                    type="date"
-                    required
-                />
-            </div>
+
             <DialogFooter className="flex flex-wrap justify-between pt-4 gap-4">
-                <DialogClose>
-                    <Button variant="destructive" type="button">
-                        Cancelar
-                    </Button>
-                </DialogClose>
-                <DialogClose>
-                    <Button variant="success" type="button">
-                        Reservar
-                    </Button>
-                </DialogClose>
+            <DialogClose asChild>
+                <Button
+                    type="button"
+                    variant="destructive">
+                    Cancelar
+                </Button>
+            </DialogClose>
+
+            {/* <DialogClose asChild> */}
+                <Button
+                    type="submit"
+                    variant="success"
+                >
+                    Reservaré
+                </Button>
+            {/* </DialogClose> */}
             </DialogFooter>
         </form>
     );
--- src/components/reservations/reservationsPage.tsx
+++ src/components/reservations/reservationsPage.tsx
@@ -1,20 +1,67 @@
-import Image from 'next/image';
+import { getReservations } from '@/app/actions/saveReservation/getReservation';
 import React from 'react';
 
-function ReservationsPage() {
+async function ReservationsPage() {
+  const reservations = await getReservations();
+
+  if (reservations.length === 0) {
+    return <div>No hay reservas disponibles.</div>;
+  }
+
   return (
-    <div className="flex flex-col">
-      <div className="min-h-screen justify-center items-center">
-        <h1>Habitaciones Familias</h1>
-        <Image
-          src="https://images.pexels.com/photos/7601116/pexels-photo-7601116.jpeg?auto=compress&cs=tinysrgb&w=800"
-          alt="Habitaciones Familiares"
-          width={720}
-          height={430}
-        />
-      </div>
+    <div className="flex flex-col min-h-screen justify-center items-center p-6 bg-gray-100">
+      <h1 className="text-3xl font-bold mb-6 text-center">Los datos de tus reservas</h1>
+
+      {reservations.map((reservation) => (
+        <div key={reservation.id} className="bg-white rounded-lg shadow-md p-6 mb-6 w-full max-w-md">
+          <div className="flex justify-between mb-4">
+            <div>
+              <h2 className="text-xl font-semibold">Entrada</h2>
+              <p className="text-gray-700">{new Date(reservation.arrivalDate).toLocaleDateString()}</p>
+            </div>
+
+            <div className="border-l border-gray-300 h-16 mx-4"></div>
+
+            <div>
+              <h2 className="text-xl font-semibold">Salida</h2>
+              <p className="text-gray-700">{new Date(reservation.departureDate).toLocaleDateString()}</p>
+            </div>
+          </div>
+
+          <div className="mb-4">
+            <h2 className="text-xl font-semibold">Duración total de la estancia:</h2>
+            <p className="text-gray-700">{calculateDuration(reservation.arrivalDate.toString(), reservation.departureDate.toString())} noche{calculateDuration(reservation.arrivalDate.toString(), reservation.departureDate.toString()) > 1 ? 's' : ''}</p>
+          </div>
+
+          <hr className="my-4 border-gray-300" />
+
+          <div className="mb-4">
+            <h2 className="text-xl font-semibold">Has seleccionado</h2>
+            <p className="text-gray-700">{reservation.rooms}: Habitación</p>
+            <p className="text-gray-700">Tipo de Habitación: {reservation.bedroomsType}</p>
+            <p className="text-gray-700">Número de huéspedes: {reservation.guests}</p>
+            <p className='text-gray-700'>Tu habitacion esta: {reservation.status}</p>
+          </div>
+
+          <div className="mb-4">
+            <h2 className="text-xl font-semibold">
+              <a href="./habitaciones" className="text-blue-500 hover:underline">
+                Cambiar tu elección
+              </a>
+            </h2>
+          </div>
+        </div>
+      ))}
     </div>
   );
 }
+
+// Función para calcular la duración de la estancia
+const calculateDuration = (arrivalDate: string, departureDate: string): number => {
+  const arrival = new Date(arrivalDate);
+  const departure = new Date(departureDate);
+  const duration = (departure.getTime() - arrival.getTime()) / (1000 * 60 * 60 * 24); // aqui convierto de milisegundos a días
+  return duration;
+};
 
 export default ReservationsPage;
