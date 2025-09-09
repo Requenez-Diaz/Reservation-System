243ccd0ef2906ebd5627474eb96849cd763a378a
Requenez <95236561+Requenez-Diaz@users.noreply.github.com>
2025-09-09 09:01:15 -0600

    [FIX]: removing fields not required for creating testimonials

--- src/app/(site)/testimonials/testimonialsSections.tsx
+++ src/app/(site)/testimonials/testimonialsSections.tsx
@@ -3,7 +3,6 @@ import { TestimonialsClient } from './testimonial-client';
 import type { Testimonial } from './type';
 
 export default async function TestimonialsSection() {
-  // Obtener testimonios reales de la base de datos
   const result = await getTestimonials();
 
   // Transformar los datos para que coincidan con la interfaz esperada
@@ -14,16 +13,11 @@ export default async function TestimonialsSection() {
         avatar: testimonial.avatar,
         rating: testimonial.rating,
         comment: testimonial.comment,
-        roomType: testimonial.roomType,
-        stayDate:
-          testimonial.stayDate instanceof Date
-            ? testimonial.stayDate.toISOString()
-            : testimonial.stayDate,
         location: testimonial.location,
         createdAt:
           testimonial.createdAt instanceof Date
-            ? testimonial.createdAt.toISOString()
-            : testimonial.createdAt,
+            ? testimonial.createdAt
+            : new Date(testimonial.createdAt),
         userId: testimonial.userId,
         User: testimonial.User
       }))
