58e50cc07d82c7cfa0da870e7f9cc0ccbd324117
Requenez <95236561+Requenez-Diaz@users.noreply.github.com>
2025-10-03 17:29:55 -0600

    [FIX]: resolving error to hidratations with nav render into server

--- src/app/(site)/layout.tsx
+++ src/app/(site)/layout.tsx
@@ -1,35 +1,19 @@
+// app/(site)/layout.tsx
 import Footer from '@/components/footer/footer';
 import NavbarTwoPass from './navbar/navbar-twoPass';
-import { Inter as FontSans } from 'next/font/google';
-import { Toaster } from '@/components/ui/toaster';
-import { cn } from '@/lib/utils';
 import { getServerSession } from 'next-auth';
 import { authOptions } from '@/lib/auth';
 
-export const fontSans = FontSans({
-  subsets: ['latin'],
-  variable: '--font-sans'
-});
-
-export default async function RootLayout({
+export default async function SiteLayout({
   children
 }: Readonly<{ children: React.ReactNode }>) {
   const session = await getServerSession(authOptions);
+
   return (
-    <html lang="es" className={fontSans.variable}>
-      <body
-        className={cn(
-          'min-h-screen bg-background font-sans antialiased',
-          fontSans.variable
-        )}
-      >
-        <div className="flex flex-col min-h-screen">
-          <NavbarTwoPass session={session} />
-          <main className="flex-grow">{children}</main>
-          <Footer />
-        </div>
-        <Toaster />
-      </body>
-    </html>
+    <div className="flex flex-col min-h-screen">
+      <NavbarTwoPass session={session} />
+      <main className="flex-grow">{children}</main>
+      <Footer />
+    </div>
   );
 }
