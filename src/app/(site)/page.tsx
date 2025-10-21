8d720b8e928ceee6ba58473e5cece36e3e9e321a
Requenez <requenez@MacBook-Pro-de-Requenez.local>
2025-10-21 17:47:39 -0600

    delete comment

--- src/app/(site)/page.tsx
+++ src/app/(site)/page.tsx
@@ -4,8 +4,6 @@ import NavbarTwoPass from './navbar/navbar-twoPass';
 const Dashboard = () => {
   return (
     <div className="">
-
-      //TODO: TEST PUSH
       <ContainerHome />
     </div>
   );
