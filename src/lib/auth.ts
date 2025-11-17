ec88229eac6fabcdabec511efa9903b1a7fb697f
Requenez <95236561+Requenez-Diaz@users.noreply.github.com>
2025-11-17 13:27:33 -0600

    fix error

--- src/lib/auth.ts
+++ src/lib/auth.ts
@@ -11,6 +11,10 @@ export const authOptions: NextAuthOptions = {
   session: {
     strategy: 'jwt'
   },
+  pages: {
+    signIn: '/sign-in',
+    signOut: '/sign-up'
+  },
   providers: [
     CredentialsProvider({
       name: 'Credentials',
