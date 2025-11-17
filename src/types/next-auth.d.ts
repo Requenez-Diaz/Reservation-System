35c43aa2ee47b5e68d073c6db240d9cdb715bfff
Requenez <95236561+Requenez-Diaz@users.noreply.github.com>
2025-11-16 19:22:22 -0600

    fix error by type in the authetications

--- src/types/next-auth.d.ts
+++ src/types/next-auth.d.ts
@@ -1,14 +1,27 @@
+import 'next-auth';
+import 'next-auth/jwt';
+
 declare module 'next-auth' {
-  interface User {
-    username: string;
+  interface Session {
+    user: {
+      id: string;
+      username: string;
+      email: string;
+      name?: string | null;
+      image?: string | null;
+    };
   }
 
-  interface Session {
-    user: User & {
-      username: string;
-    };
-    token: {
-      username: string;
-    };
+  interface User {
+    id: string;
+    username: string;
+    email: string;
+  }
+}
+
+declare module 'next-auth/jwt' {
+  interface JWT {
+    id: string;
+    username: string;
   }
 }
